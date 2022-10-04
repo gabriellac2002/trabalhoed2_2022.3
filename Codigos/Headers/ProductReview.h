0f207166f7befcbfa769654d238c1ee2160c7b82
Mariana <maryhsiano@hotmail.com>
2022-10-04 17:41:50 -0300

    Adicionando as outras funcoes na ProductReview.h

--- Codigos/Headers/ProductReview.h
+++ Codigos/Headers/ProductReview.h
@@ -5,10 +5,15 @@ class ProductReview
 {
     private:
         int tamanho;
+
     public:
         ProductReview(int n);
 
         void print();
+        void createBinary(string& path);
+        void getReview(int i);
+
+        ProductReview* import(int n);
 };
 
 #endif // ProductReview_H_INCLUDED
