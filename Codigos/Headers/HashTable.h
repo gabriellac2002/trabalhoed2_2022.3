b64fd3e8e8834d81bd88b4e9aac0587498b9fc6e
Mariana <maryhsiano@hotmail.com>
2022-11-14 12:43:28 -0300

    Parte do cpp terminada

--- Codigos/Headers/HashTable.h
+++ Codigos/Headers/HashTable.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <bitset>
 
+#include "../Headers/ProductReview.h"
+
 using namespace std;
 
 #define Max_Size 130
--- Codigos/source/HashTable.cpp
+++ Codigos/source/HashTable.cpp
@@ -19,3 +19,61 @@ HashTable::HashTable(unsigned int tam, string enderecoArqBin)
         table[i] = nullptr; // Inicializando todas as posições como nullptr
 }
 
+// Conferir a desalocação da tabela
+HashTable::~HashTable()
+{
+    for(int i = 0; i < tamanho; ++i) {
+        ProductReview *entrada = table[i];
+        ProductReview *proximo = table[i + 1];
+
+        while (entrada != nullptr) // percorre por todos os registros contidos no mesmo endereço da Tabela Hash e os desaloca
+        {
+            ProductReview *anterior = entrada;
+            //entrada = entrada->proximo
+            delete anterior;
+        }
+        
+    }
+
+    delete [] table;
+}
+
+int HashTable::convertToInt(char *id)
+{
+    int valor, tam;
+    valor = 7;
+    tam = strlen(id);
+
+    for(int i = 0; i < tam; i++) {
+        valor = 31 * valor + (int)id[i];
+    }
+
+    return valor;
+}
+
+void HashTable::inserir(char *id)
+{
+
+}
+
+void HashTable::printHash()
+{
+    ProductReview **aux = table;
+    ProductReview *no;
+
+    for(int i = 0; i < tamanho; i++) {
+        no = aux[i];
+
+        if(no != nullptr) {
+            while (no != nullptr)
+            {
+                cout << "Posiçãp da Hash: " << i << "; ID do usuário: " << no->getUserId() << endl;
+                //Para a proxima posicao da tabela
+            }
+            
+        }
+
+        else
+            cout << "Posição da Hash: " << i << " = NULL" << endl;
+    }
+}
