2acaa9dc1911078fc8ab9842f0d7d2786d0c0805
Mariana <maryhsiano@hotmail.com>
2022-11-14 11:27:35 -0300

    'HashTable.h' refeita

---
+++ Codigos/Headers/HashTable.h
@@ -0,0 +1,34 @@
+#ifndef HashTable_H_INCLUDED
+#define HashTable_H_INCLUDED
+
+#include <fstream>
+#include <vector>
+#include <bitset>
+
+using namespace std;
+
+#define Max_Size 130
+
+class HashTable
+{
+    private:
+        ProductReview **table;
+        unsigned int tamanho;
+        string enderecoArqBin;
+        int tamVetHash;
+
+    public:
+        HashTable(unsigned int, string enderecoArqBin);
+        ~HashTable();
+
+        int get(int key);
+        void inserir(char *id);
+        void printHash();
+        int convertToInt(char *id);
+        int busca(char *id);
+        void maisFreq(int m, bool teste);
+        void quickHash(ProductReview **vet, int p, int r);
+        int particionamentoHash(ProductReview **vet, int p, int q);
+};
+
+#endif // HashTable_H_INCLUDED
---
+++ Codigos/source/HashTable.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+
+#include "../Headers/HashTable.h"
+#include "../Headers/ProductReview.h"
+
+using namespace std;
