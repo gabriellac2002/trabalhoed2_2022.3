6c188d43d380872956a4ea593961a8e625853b45
Mariana <maryhsiano@hotmail.com>
2023-01-02 16:24:11 -0300

    Arquivo no da arvore B

---
+++ Codigos/Headers/BTreeNode.h
@@ -0,0 +1,11 @@
+#ifndef BTreeNode_H_INCLUDED
+#define BTreeNode_H_INCLUDED
+
+using namespace std;
+
+class BTreeNode
+{
+    
+};
+
+#endif // BTreeNode_H_INCLUDED
--- Codigos/Headers/TreeB.h
+++
@@ -1,6 +0,0 @@
-#ifndef TreeB_H_INCLUDED
-#define TreeB_H_INCLUDED
-
-using namespace std;
-
-#endif // TreeB_H_INCLUDED
