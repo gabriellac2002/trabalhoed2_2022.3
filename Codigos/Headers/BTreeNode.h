70772c7c92d75d82b6ebd64b19265d727267187c
Mariana <maryhsiano@hotmail.com>
2023-01-02 16:31:43 -0300

    private do no folha feito

--- Codigos/Headers/BTreeNode.h
+++ Codigos/Headers/BTreeNode.h
@@ -5,7 +5,16 @@ using namespace std;
 
 class BTreeNode
 {
-    
+    private:
+        int *keys; //Um array de chaves
+        int t; // Grau minimo (define a faixa de nº de chaves)
+
+        BTreeNode **C; //Uma matriz de ponteiros filhos
+        int n; //Número atual de chaves
+        bool leaf; //É verdadeiro quando o nó é folha. Caso contrário, é falso
+
+    public:
+        
 };
 
 #endif // BTreeNode_H_INCLUDED
