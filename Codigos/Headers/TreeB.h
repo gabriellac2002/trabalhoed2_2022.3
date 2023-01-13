ee0a9ebb2f4122b6f158ea6aa2be422ed6a56a3b
Mariana <maryhsiano@hotmail.com>
2023-01-13 20:17:52 -0300

    'TreeB.h' feita

---
+++ Codigos/Headers/TreeB.h
@@ -0,0 +1,38 @@
+#ifndef TREEB_H_INCLUDED
+#define TREEB_H_INCLUDED
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sys/stat.h>
+#include <algorithm>
+
+#include "TreeBNo.h" //incluindo o '.h' do Nó da Árvore B
+
+using namespace std;
+
+class TreeB 
+{
+    private:
+        TreeBNo *raiz; //Ponteiro para o nó raiz
+        int t; //grau minimo
+
+    public:
+        TreeB(int grau); //Construtor
+
+        //Função para percorrer a árvore
+        void percorrer()
+        { if(raiz != NULL) raiz->percorrer(); }
+
+        //Função para buscar chave na árvore
+        TreeBNo *buscar(int k)
+        {return (raiz == NULL)? NULL : raiz->buscar(k); }
+
+        // A função main que insere uma nova chave
+        void inserir(int k); 
+};
+
+#endif //TREEB_H_INCLUDED
